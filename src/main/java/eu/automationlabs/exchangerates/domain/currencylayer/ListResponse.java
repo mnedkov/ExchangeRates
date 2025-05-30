ed54c3b57abb6ac0b95d31fd0f4a197c220d645b
Mihail Nedkov <mihail.nedkov.ext@sodexo.com>
2025-05-30 10:19:46 +0300

    Fix inheritance structure: Make LiveResponse and ListResponse extend BaseResponse

--- src/main/java/eu/automationlabs/exchangerates/domain/currencylayer/ListResponse.java
+++ src/main/java/eu/automationlabs/exchangerates/domain/currencylayer/ListResponse.java
@@ -1,20 +1,38 @@
 package eu.automationlabs.exchangerates.domain.currencylayer;
 
-import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ListResponse extends BaseResponse {
+    
+    private String terms;
+    private String privacy;
+    private Map<String, String> currencies;
 
-	private Map<String, String> currencies;
+    
 
-	public Map<String, String> getCurrencies() {
-		return currencies;
-	}
+    public String getTerms() {
+        return terms;
+    }
 
-	public void setCurrencies(Map<String, String> currencies) {
-		this.currencies = currencies;
-	}
+    public void setTerms(String terms) {
+        this.terms = terms;
+    }
 
+    public String getPrivacy() {
+        return privacy;
+    }
+
+    public void setPrivacy(String privacy) {
+        this.privacy = privacy;
+    }
+
+    public Map<String, String> getCurrencies() {
+        return currencies;
+    }
+
+    public void setCurrencies(Map<String, String> currencies) {
+        this.currencies = currencies;
+    }
 }
--- src/main/java/eu/automationlabs/exchangerates/domain/currencylayer/LiveResponse.java
+++ src/main/java/eu/automationlabs/exchangerates/domain/currencylayer/LiveResponse.java
@@ -1,27 +1,56 @@
 package eu.automationlabs.exchangerates.domain.currencylayer;
 
-import java.util.Map;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Map;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LiveResponse extends BaseResponse {
-	private String source;
-	private Map<String, Double> quotes;
+    
+    private String terms;
+    private String privacy;
+    private long timestamp;
+    private String source;
+    private Map<String, Double> quotes;
 
-	public String getSource() {
-		return source;
-	}
+    
 
-	public void setSource(String source) {
-		this.source = source;
-	}
+    public String getTerms() {
+        return terms;
+    }
 
-	public Map<String, Double> getQuotes() {
-		return quotes;
-	}
+    public void setTerms(String terms) {
+        this.terms = terms;
+    }
 
-	public void setQuotes(Map<String, Double> quotes) {
-		this.quotes = quotes;
-	}
+    public String getPrivacy() {
+        return privacy;
+    }
+
+    public void setPrivacy(String privacy) {
+        this.privacy = privacy;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public Map<String, Double> getQuotes() {
+        return quotes;
+    }
+
+    public void setQuotes(Map<String, Double> quotes) {
+        this.quotes = quotes;
+    }
 }
